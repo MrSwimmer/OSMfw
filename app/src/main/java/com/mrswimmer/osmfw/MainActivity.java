07c93cfb21ec7c29fc45daa1ba65a113366cad9b
MrSwimmer <mrswimmerlab@gmail.com>
2017-12-08 00:42:30 +0700

    v1.1 correct work without saving last result

--- app/src/main/java/com/mrswimmer/osmfw/MainActivity.java
+++ app/src/main/java/com/mrswimmer/osmfw/MainActivity.java
@@ -5,16 +5,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
     LinearLayout get, info;
+    ImageView im1, im2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        get = (LinearLayout) findViewById(R.id.menu_get);
-        info = (LinearLayout) findViewById(R.id.menu_info);
+        setContentView(R.layout.activity_maint);
+        get = findViewById(R.id.menu_get);
+        info = findViewById(R.id.menu_info);
+        im1 = findViewById(R.id.menu_im1);
+        im2 = findViewById(R.id.menu_im2);
+        im1.setImageResource(R.drawable.nnote);
+        im2.setImageResource(R.drawable.statist);
         get.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/mrswimmer/osmfw/OSM.java
+++ app/src/main/java/com/mrswimmer/osmfw/OSM.java
@@ -11,6 +11,7 @@ import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,14 +22,14 @@ public class OSM extends Activity implements SensorEventListener {
     //0 - not fix
     //1 - wait
     //2 - result
-    int status=0;
+    int status=0, P1, P2, zone;
     private SensorManager mSensorManager;
     private Sensor mHeartRateSensor;
-
+    private double point, x = 2.23, y = 0.5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_osm);
+        setContentView(R.layout.activity_osmt);
         button = findViewById(R.id.osm_but);
         pulse = findViewById(R.id.osm_pulse);
         text = findViewById(R.id.osm_text);
@@ -40,16 +41,26 @@ public class OSM extends Activity implements SensorEventListener {
                 switch (status) {
                     case 0 :
                         status = 1;
+                        P1 = Integer.parseInt(pulse.getText().toString());
+                        Log.i("osm", "status 0 "+ P1+"");
                         text.setText(R.string.osm_wait);
                         new Handler().postDelayed(new Runnable() {
                             @Override
                             public void run() {
-
+                                P2 = Integer.parseInt(pulse.getText().toString());
+                                point = (float) (14.5 - y * ((float) (P1) - 40) / 3.5 - ((float) (P2 - P1)) / x * 0.5);
+                                zone = getZone();
+                                status = 2;
+                                String buf = String.format("%.2f", point);
+                                pulse.setText(buf+" б"+"\n"+zone+ "зона");
+                                text.setText("Измерить еще раз");
                             }
-                        }, 1000);
+                        }, 7000);
                         break;
                     case 2 :
-
+                        Log.i("osm", "status 2 ");
+                        status = 0;
+                        text.setText(R.string.osm_fix);
                 }
             }
         });
@@ -58,7 +69,7 @@ public class OSM extends Activity implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        if (sensorEvent.sensor.getType() == Sensor.TYPE_HEART_RATE) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_HEART_RATE && (status == 0 || status == 1)) {
             pulse.setText((int) sensorEvent.values[0]+"");
         }
     }
@@ -86,5 +97,17 @@ public class OSM extends Activity implements SensorEventListener {
         //Unregister the listener
         if (mSensorManager!=null)
             mSensorManager.unregisterListener(this);
+    }
+    public int getZone() {
+        int n = 0;
+        if (point >= 7.5)
+            n = 1;
+        if (point >= 5 && point < 7.5)
+            n = 2;
+        if (point >= 2.5 && point < 5)
+            n = 3;
+        if (point < 2.5)
+            n = 4;
+        return n;
     }
 }
